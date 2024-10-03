a6009e773ff9df80cec78663fb177d4bb1dc5a0c
Utkarsh <ignition3951@gmail.com>
2024-10-03 14:00:06 +0530

    UNderstand flat map

---
+++ Java8InAction/src/com/utk/action/Chapter5.java
@@ -0,0 +1,26 @@
+package com.utk.action;
+
+import java.util.Arrays;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+public class Chapter5 {
+
+	public static void main(String[] args) {
+		List<Integer> numbers1 = Arrays.asList(1,2,3);
+		List<Integer> numbers2 = Arrays.asList(4,5);
+		
+		//Create a output {i,j} having mapped both the lists
+		List<int[]> mapped12=
+				numbers1.stream()
+				.flatMap(list-> numbers2.stream()//to move inside a list of list
+						.map(j->new int[] {list,j}))
+				.collect(toList());
+
+		for(int i=0;i<mapped12.size();i++) {
+				System.out.println("("+mapped12.get(i)[0]+","+mapped12.get(i)[1]+")");
+		}
+
+	}
+
+}
