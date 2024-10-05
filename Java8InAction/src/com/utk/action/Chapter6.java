4f13b8531bf1fc67371b66bc48f90354d3a1808a
Utkarsh <ignition3951@gmail.com>
2024-10-05 12:50:21 +0530

    IMplement grouping on stream

--- Java8InAction/src/com/utk/action/Chapter6.java
+++ Java8InAction/src/com/utk/action/Chapter6.java
@@ -3,6 +3,7 @@ package com.utk.action;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static java.util.stream.Collectors.*;
@@ -17,16 +18,32 @@ public class Chapter6 {
 				new Dish("french fries", true, 530, Dish.Type.OTHER), new Dish("rice", true, 350, Dish.Type.OTHER),
 				new Dish("season fruit", true, 120, Dish.Type.OTHER), new Dish("pizza", true, 550, Dish.Type.OTHER),
 				new Dish("prawns", false, 300, Dish.Type.FISH), new Dish("salmon", false, 450, Dish.Type.FISH));
-		
-		Comparator<Dish> dishCaloriesComparator = 
-				Comparator.comparingInt(Dish::getCalories);
-		
-		Optional<Dish> mostCalorieDish=
-				menu.stream().collect(maxBy(dishCaloriesComparator));
-		
-		if(mostCalorieDish.isPresent()) {
-			System.out.println("mostCalorieDish : "+mostCalorieDish);
+
+		Comparator<Dish> dishCaloriesComparator = Comparator.comparingInt(Dish::getCalories);
+
+		Optional<Dish> mostCalorieDish = menu.stream().collect(maxBy(dishCaloriesComparator));
+
+		if (mostCalorieDish.isPresent()) {
+			System.out.println("mostCalorieDish : " + mostCalorieDish);
 		}
+
+		// Grouping dishes by type
+		Map<Dish.Type, List<Dish>> groupDishesByType = menu.stream().collect(groupingBy(Dish::getType));
+		System.out.println("groupDishesByType : " + groupDishesByType.toString());
+
+		// Grouping by diet classification
+		Map<CalorieLevel, List<Dish>> groupByDietLevel=
+				menu.stream().collect(groupingBy(calorie->{
+					if(calorie.getCalories()<=400) return CalorieLevel.DIET;
+					else if(calorie.getCalories()<=700) return CalorieLevel.NORMAL;
+					else return CalorieLevel.FAT;
+				}));
+		System.out.println("groupByDietLevel : " + groupByDietLevel);
+
+	}
+
+	public enum CalorieLevel {
+		DIET, NORMAL, FAT
 	}
 
 }
