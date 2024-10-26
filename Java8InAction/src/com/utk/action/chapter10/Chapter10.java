419ad060e814e012e9039276154e8c04f277aa19
Utkarsh <ignition3951@gmail.com>
2024-10-26 13:07:07 +0530

    Implement use cases of Optional class

--- Java8InAction/src/com/utk/action/chapter10/Chapter10.java
+++ Java8InAction/src/com/utk/action/chapter10/Chapter10.java
@@ -1,6 +1,9 @@
 package com.utk.action.chapter10;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 
 import com.utk.model.Car;
 import com.utk.model.Insurance;
@@ -14,6 +17,26 @@ public class Chapter10 {
 
 		Optional<Person> dummy2 = Optional.ofNullable(null);
 		System.out.println(getCarInsuranceName(dummy2));
+		
+		Map<String, String> dummy3 = new HashMap<String, String>();
+		dummy3.put("a", "1");
+		
+		//before using optional
+		System.out.println(dummy3.get("b"));
+		
+		Optional<Object> optionalDummy3= Optional.ofNullable(dummy3.get("b"));
+		//After using optional on legacy java API
+		System.out.println(optionalDummy3);
+		
+		Properties props=new Properties();
+		props.setProperty("a", "5");
+		props.setProperty("b", "true");
+		props.setProperty("c", "-23");
+		
+		System.out.println(readDuration(props,"a"));
+		System.out.println(readDuration(props, "b"));
+		System.out.println(readDuration(props, "c"));
+		
 	}
 
 	// Used flatmap because map will return Optional<Optional<Person>> which wont
@@ -32,5 +55,23 @@ public class Chapter10 {
 	public static Insurance findCheapestInsurance(Person person, Car car) {
 		Insurance insurance = null;
 		return insurance;
+	}
+	
+	//Read only positive values modified with the help of Optional class
+	public static int readDuration(Properties props,String name) {
+		return Optional.ofNullable(props.getProperty(name))
+				.flatMap(Chapter10::stringToInt)
+				.filter(i->i>0)
+				.orElse(0);
+		
+	}
+	
+	//Legacy Integer.parseInt is modified with the help of Optional
+	public static Optional<Integer> stringToInt(String s) {
+		try {
+			return Optional.of(Integer.parseInt(s));
+		} catch (NumberFormatException e) {
+			return Optional.empty();
+		}
 	}
 }
