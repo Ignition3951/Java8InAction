17feff03c8b447f6e0cd05d9cb03df5b79ede67a
Utkarsh <ignition3951@gmail.com>
2024-11-19 12:20:15 +0530

    IMplement first and third day functionality using java 8 date and time

--- Java8InAction/src/com/utk/action/chapter12/Chapter12.java
+++ Java8InAction/src/com/utk/action/chapter12/Chapter12.java
@@ -1,6 +1,8 @@
 package com.utk.action.chapter12;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 import com.utk.model.NextWorkingDay;
 
@@ -10,6 +12,14 @@ public class Chapter12 {
 		LocalDate date = LocalDate.of(2024, 11, 01);
 		date = date.with(new NextWorkingDay());
 		System.out.println(date);
+		
+		LocalDate today = LocalDate.now();
+		LocalDate firstSaturdayOfMonth = today.with(TemporalAdjusters.firstInMonth(DayOfWeek.TUESDAY));
+		LocalDate thirdSaturdayOfMonth = firstSaturdayOfMonth.plusDays(14);
+		System.out.println(firstSaturdayOfMonth+"    "+thirdSaturdayOfMonth);
+		if(today.equals(firstSaturdayOfMonth) || today.equals(thirdSaturdayOfMonth)) {
+			System.out.println("True");
+		}
 	}
 
 }
