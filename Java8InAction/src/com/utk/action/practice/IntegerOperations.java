1eb186b29d7dc9b05c44ea40c232a9de1f0a0e7a
Utkarsh <ignition3951@gmail.com>
2024-12-12 13:32:54 +0530

    JAVA8-6 Implement max and min number

--- Java8InAction/src/com/utk/action/practice/IntegerOperations.java
+++ Java8InAction/src/com/utk/action/practice/IntegerOperations.java
@@ -1,6 +1,7 @@
 package com.utk.action.practice;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class IntegerOperations {
@@ -10,6 +11,14 @@ public class IntegerOperations {
 		List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
 		
 		listOfIntegers.stream().filter(number-> number %5==0).forEach(number-> System.out.println(number));//multiples of 5
+		
+		Integer maxNumber = listOfIntegers.stream().max(Comparator.naturalOrder()).get();
+		
+		Integer minNumber = listOfIntegers.stream().min(Comparator.naturalOrder()).get();
+		
+		System.out.println("Maximum number is : "+maxNumber+" minimum number is : "+minNumber);
+		
+		
 
 	}
 
