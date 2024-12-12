7fa6a670cbcc4a041dfa9a940a4d58119ee9a0e5
Utkarsh <ignition3951@gmail.com>
2024-12-12 13:40:15 +0530

    JAVA8-6 Implement duplicate resolution after concatenation

--- Java8InAction/src/com/utk/action/practice/IntegerOperations.java
+++ Java8InAction/src/com/utk/action/practice/IntegerOperations.java
@@ -3,6 +3,7 @@ package com.utk.action.practice;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class IntegerOperations {
 
@@ -18,7 +19,17 @@ public class IntegerOperations {
 		
 		System.out.println("Maximum number is : "+maxNumber+" minimum number is : "+minNumber);
 		
+		int[] listOfIntegersOne = {45, 12, 56, 15, 24, 75, 31, 89};
 		
+		int[] listOfIntegersTwo = {45, 12, 56, 15, 24, 75, 31, 89};
+		
+		int[] concatList = IntStream.concat(Arrays.stream(listOfIntegersOne), Arrays.stream(listOfIntegersTwo)).toArray();
+		
+		System.out.println("Concatenated string with duplicates is : "+Arrays.toString(concatList));
+		
+		int[] concatListWithoutDuplicates = IntStream.concat(Arrays.stream(listOfIntegersOne), Arrays.stream(listOfIntegersTwo)).sorted().distinct().toArray();
+		
+		System.out.println("Concatenated string without duplicates is : "+Arrays.toString(concatListWithoutDuplicates));
 
 	}
 
