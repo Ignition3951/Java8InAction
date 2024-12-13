d92c669c64c02ae359ac1193c97e39b68404a979
Utkarsh <ignition3951@gmail.com>
2024-12-13 21:29:47 +0530

    Implement argument check

--- Java8InAction/src/com/utk/action/prospring6/chapter2/HelloWorld.java
+++ Java8InAction/src/com/utk/action/prospring6/chapter2/HelloWorld.java
@@ -3,7 +3,12 @@ package com.utk.action.prospring6.chapter2;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!!!!!!!");
+		if (args.length > 0) {
+			System.out.println(args[0]);
+		} else {
+			System.out.println("Hello World!!!!!!!!!");
+		}
+
 	}
 
 }
